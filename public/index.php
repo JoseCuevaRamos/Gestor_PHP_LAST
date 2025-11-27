93288eef3ebc067b3269ea70595ca6238bd1d3dd
Jose Cueva <josenil2002@gmail.com>
2025-11-27 03:43:32 -0500

    4

---
+++ public/index.php
@@ -0,0 +1,38 @@
+<?php
+if (PHP_SAPI == 'cli-server') {
+    // To help the built-in PHP dev server, check if the request was actually for
+    // something which should probably be served as a static file
+    $url  = parse_url($_SERVER['REQUEST_URI']);
+    $file = __DIR__ . $url['path'];
+    if (is_file($file)) {
+        return false;
+    }
+}
+
+require __DIR__ . '/../vendor/autoload.php';
+
+session_start();
+
+// Instantiate the app
+$settings = require __DIR__ . '/../src/settings.php';
+$app = new \Slim\App($settings);
+
+// Set up dependencies
+require __DIR__ . '/../src/dependencies.php';
+
+// Register middleware
+require __DIR__ . '/../src/middleware.php';
+
+// Register routes
+require __DIR__ . '/../src/routes.php';
+
+// Run app
+try {
+    $app->run();
+} catch (\Throwable $e) {
+    // Escribe el error completo en el registro de PHP
+    error_log($e->__toString());
+
+    // Opcional: También puedes devolver una respuesta de error si lo deseas,
+    // pero para la depuración, registrar el error es lo más importante.
+}
